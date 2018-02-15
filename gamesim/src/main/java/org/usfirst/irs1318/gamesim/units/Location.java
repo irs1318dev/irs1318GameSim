cd7cada340b1338a0b52b975e0dc56a37ae26692
aedans <aedan.smith@outlook.com>
2018-02-14 19:16:43 -0800

    Standardize units

--- gamesim/src/main/java/org/usfirst/irs1318/gamesim/units/Location.java
+++ gamesim/src/main/java/org/usfirst/irs1318/gamesim/units/Location.java
@@ -1,4 +1,4 @@
-package org.usfirst.irs1318.gamesim.util;
+package org.usfirst.irs1318.gamesim.units;
 
 import java.util.Objects;
 
@@ -7,8 +7,8 @@ import java.util.Objects;
  *
  * A game can be treated as though it were on a graph. The
  * origin is at the bottom left of the arena, and the scale
- * is three feet per one unit. This location represents
- * a point on that graph.
+ * is one meter per one unit. This location represents a point
+ * on that graph.
  *
  * Locations should not be compared directly. Instead, they
  * should be checked to see if they're in an {@link Area}.
@@ -22,20 +22,6 @@ public final class Location {
         this.y = y;
     }
 
-    /**
-     * Translates a location in any {@link Direction}.
-     * @param distance The distance to translate the location.
-     */
-    public Location translate(Direction direction, float distance) {
-        switch (direction) {
-            case LEFT: return new Location(x - distance, y);
-            case RIGHT: return new Location(x + distance, y);
-            case UP: return new Location(x, y + distance);
-            case DOWN: return new Location(x, y - distance);
-            default: throw new IllegalArgumentException(direction.toString());
-        }
-    }
-
     public float getX() {
         return x;
     }
@@ -46,7 +32,10 @@ public final class Location {
 
     @Override
     public String toString() {
-        return "(" + x + ", " + y + ")";
+        return "Location{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 
     @Override
--- gamesim/src/main/java/org/usfirst/irs1318/gamesim/util/Direction.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.irs1318.gamesim.util;
-
-/**
- * Enum that represents the cardinal directions.
- */
-public enum Direction {
-    LEFT, RIGHT, UP, DOWN
-}
