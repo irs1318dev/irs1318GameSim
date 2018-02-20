723d8d86dc395b07cbed413aa405bcda3e150ef7
SunnyYuan2017 <32754694+SunnyYuan2017@users.noreply.github.com>
2018-02-19 20:40:44 -0800

    Add PrettyPrint

--- model/src/test/java/org/usfirst/irs1318/model/powerup2018/FoulTracker.java
+++ model/src/test/java/org/usfirst/irs1318/model/powerup2018/FoulTracker.java
@@ -1,7 +1,9 @@
 package org.usfirst.irs1318.model.powerup2018;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.usfirst.irs1318.model.Actor;
+import org.usfirst.irs1318.model.Alliance;
 import org.usfirst.irs1318.model.ListensTo;
 
 import java.util.HashSet;
@@ -9,29 +11,67 @@ import java.util.Set;
 
 public class FoulTracker {
     @Test
-    public void buildG23FoulTracker(){
+    public void buildG2FoulTracker() throws Exception {
 
         Set<ListensTo> listensTo = new HashSet<>();
 
-            listensTo.add(
+        listensTo.add(
                 new ListensTo.Builder()
-                .setActorName("R1")
-                .build());
+                        .setActorName("R1")
+                        .build());
+        listensTo.add(
+                new ListensTo.Builder()
+                        .setActorName("R2")
+                        .build());
+        listensTo.add(
+                new ListensTo.Builder()
+                        .setActorName("R3")
+                        .build());
+        listensTo.add(
+                new ListensTo.Builder()
+                        .setActorName("B1")
+                        .build());
+
+        listensTo.add(
+                new ListensTo.Builder()
+                        .setActorName("B2")
+                        .build());
+
+        listensTo.add(
+                new ListensTo.Builder()
+                        .setActorName("B3")
+                        .build());
 
 
 
 
-
-
-
-
-
-        Actor g23FoulTracker =
+        Actor g23RedFoulTracker =
                 new Actor.Builder()
-                .setName("G23FoulTracker")
-                .setListensTo(listensTo)
-                .build();
+                        .setName("G23RedFoulTracker")
+                        .setType("FoulTracker")
+                        .setAlliance(Alliance.RED)
+                        .setListensTo(listensTo)
+                        .build();
+        Actor g23BlueFoulTracker =
+                new Actor.Builder()
+                        .setName("G23BlueFoulTracker")
+                        .setType("FoulTracker")
+                        .setAlliance(Alliance.BLUE)
+                        .setListensTo(listensTo)
+                        .build();
 
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String value = objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(g23RedFoulTracker);
+        System.out.println(value);
+
+        value = objectMapper
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(g23BlueFoulTracker);
+        System.out.println(value);
     }
 
 
