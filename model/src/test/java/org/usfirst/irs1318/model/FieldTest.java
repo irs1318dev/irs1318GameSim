26134be423bc7a67ace116d595b9bdb5aefedc98
CaitlynM <cam0417.01@gmail.com>
2018-02-20 20:48:18 -0800

    Added GameTerms, FieldSquares, and Field

---
+++ model/src/test/java/org/usfirst/irs1318/model/FieldTest.java
@@ -0,0 +1,26 @@
+package org.usfirst.irs1318.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FieldTest {
+    @Test
+    public void testBuilder(){
+        Field field = new Field.Builder()
+                .build();
+    }
+    @Test
+    public void testBuilderWithJackson()throws Exception{
+
+        Field field = new Field.Builder()
+                .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String value = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(field);
+        System.out.println(value);
+        Field fieldValue = objectMapper.readValue(value, Field.class);
+        System.out.println(fieldValue.toString());
+        assertEquals(field, fieldValue);
+    }
+}
